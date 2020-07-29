80736c9fdcc4acbc4d5195b44ab0cb3c3c664fa3
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-07-29 11:58:47 +0200

    Initial project added

---
+++ src/Pokemon.js
@@ -0,0 +1,26 @@
+const Pokemon =({id}) =>{
+  return null;
+
+  return !pokemon ? (
+    <div>{error}</div>
+  ) : ( 
+    <div>
+      <h2>#{id}</h2>
+      <h2>{pokemon.name}</h2>
+      <img alt="pokemon" src={pokemon.sprites.front_shiny} />
+    </div>
+  );
+};
+
+export const PokemonPager =() =>{
+  const [id,setId] =useState(1);
+
+  return(
+    <div>
+      <button type="button" onClick ={() => setId(id !== 1 ? id-1)}> Previous </button>
+      <button type="button" onClick ={() => setId(id !== 250 ? id+1)}>Previous</button>
+
+      <Pokemon id={id} />
+    </div>
+  )
+}
